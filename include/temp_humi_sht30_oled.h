75a1e4dfa4315d8a60eeb6cc644b3de121945a84
phanlehau404 <phanlehau404@gmail.com>
2025-12-10 20:22:43 +0700

    Hau add /temp_humi_sht30_oled.h

---
+++ include/temp_humi_sht30_oled.h
@@ -0,0 +1,7 @@
+#ifndef TEMP_HUMI_SHT30_OLED_H
+#define TEMP_HUMI_SHT30_OLED_H
+
+// FreeRTOS task function for temperature & humidity monitoring with SHT30 sensor and OLED display
+void temp_humi_sht30_oled(void *parameter);
+
+#endif
